39d62fe12af4c70215595954697adf0c9a34bc3e
Ariel Elkin <ariel@arivibes.com>
2013-05-18 21:45:42 +0100

    initial commit

---
+++ BionicLooper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BionicLooper
+//
+//  Created by Ariel Elkin on 18/05/2013.
+//  Copyright (c) 2013 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BionicLooper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BionicLooper
+//
+//  Created by Ariel Elkin on 18/05/2013.
+//  Copyright (c) 2013 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
